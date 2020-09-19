e3744e8ae3114b77b11df1f6714191e8b1c9c4e3
Sascha Mann <git@mail.saschamann.eu>
2020-09-19 15:36:14 +0200

    Initial commit
    Before querying API for repo info
    
    Prototype
    
    Rename to index
    
    rm README
    
    Open in New Tab
    
    Goify everything
    
    Add time of last update
    
    Add number of participating projects
    
    Create publish.yml
    
    Update publish.yml
    
    Drop noreferrer
    
    Rebuild & deploy every 3 hours
    
    Format
    
    Disable jekyll
    
    Only search open issues 🤦‍♂️
    
    Sort by 'updated'
    
    Add class to repos
    
    Rename to your-first-julia-pr
    
    Add licence
    
    Rename

---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/google/go-github/v32/github"
+	"golang.org/x/oauth2"
+	"html/template"
+	"log"
+	"math"
+	"os"
+	"strings"
+	"time"
+)
+
+type RepoInfo struct {
+	URL         string
+	IssueCount  int
+	Name        string
+	Description string
+}
+
+func now() string {
+	return time.Now().Format(time.RFC3339)
+}
+
+func main() {
+	// Define the UI
+	var token, outflag string
+	flag.StringVar(&token, "t", "", "GitHub Token")
+	flag.StringVar(&outflag, "o", "", "Path to the rendered template")
+	flag.Parse()
+
+	var out *os.File
+	var err error
+	if outflag == "" {
+		out = os.Stdout
+	} else {
+		out, err = os.Create(outflag)
+		if err != nil {
+			log.Fatalf("Could not open %v: %v", outflag, err)
+		}
+	}
+
+	// Setup GitHub client
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	opts := &github.SearchOptions{
+		Sort:        "updated",
+		ListOptions: github.ListOptions{PerPage: 100, Page: 1},
+	}
+
+	// Search for issues labelled "Hacktoberfest" across Julia repos
+	var issues []*github.Issue
+	for {
+		res, resp, err := client.Search.Issues(ctx, "is:issue is:open language:julia label:hacktoberfest", opts)
+		if err != nil {
+			log.Fatalf("Could not execute search: %v", err)
+		} else if resp.StatusCode != 200 {
+			log.Fatalf("Search failed: %v - %v", resp.StatusCode, resp.Status)
+		}
+
+		log.Printf("Searching page %v/%v...", opts.Page, int(math.Ceil(float64(res.GetTotal())/float64(100))))
+
+		issues = append(issues, res.Issues...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opts.Page = resp.NextPage
+	}
+
+	repos := make(map[string]int)
+
+	for _, issue := range issues {
+		repos[issue.GetRepositoryURL()]++
+	}
+
+	// Create entries for the list of participating repos
+	var entries []RepoInfo
+	for repoURL, count := range repos {
+		// Get repo data from API
+		splitURL := strings.Split(repoURL, "/")
+		owner := splitURL[len(splitURL)-2]
+		repo := splitURL[len(splitURL)-1]
+
+		log.Printf("Fetching repo info for %v/%v...", owner, repo)
+		res, resp, err := client.Repositories.Get(ctx, owner, repo)
+		if err != nil {
+			log.Fatalf("Could not get repo: %v", err)
+		} else if resp.StatusCode != 200 {
+			log.Fatalf("Repo lookup failed: %v - %v", resp.StatusCode, resp.Status)
+		}
+
+		entries = append(entries, RepoInfo{
+			URL:         res.GetHTMLURL(),
+			IssueCount:  count,
+			Name:        res.GetFullName(),
+			Description: res.GetDescription(),
+		})
+	}
+
+	// Render template
+	funcMap := template.FuncMap{"now": now}
+	log.Println("Parsing template...")
+	tpl, err := template.New("template.html").Funcs(funcMap).ParseFiles("template.html")
+	if err != nil {
+		log.Fatalf("Could not parse template: %v", err)
+	}
+
+	log.Println("Executing template...")
+	err = tpl.Execute(out, entries)
+	if err != nil {
+		log.Fatalf("Could not execute template: %v", err)
+	}
+}
